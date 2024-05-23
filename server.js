c8bc7ce5e8f175ef4cbcd6c86b163f6124b557b4
Ayflow350 <soluwatist@gmail.com>
2024-05-23 02:38:22 +0100

    bluef

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://dailycare.vercel.app/"],
+    origin: ["http://localhost:3000", "https://dailycare.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
   })
 );
